ba0d9945a5b6df848b19bae6f4f705496f88d50c
Sumera24 <sumeraqz@gmail.com>
2020-12-08 13:20:06 -0500

    From Workspace Third

--- src/module1/FisrtClass.java
+++ src/module1/FisrtClass.java
@@ -5,6 +5,8 @@ public class FisrtClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
   System.out.println("Sumera Quazi");
+  System.out.println("Sumera Quazi");
+  System.out.println("Sumera Quazi");
 	}
 
 }
