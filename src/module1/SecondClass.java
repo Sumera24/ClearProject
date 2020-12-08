84ba69ab556e104a3de3046af14924ddfcd4823e
Sumera24 <sumeraqz@gmail.com>
2020-12-08 13:52:27 -0500

    Third Workspace

--- src/module1/SecondClass.java
+++ src/module1/SecondClass.java
@@ -6,6 +6,7 @@ public class SecondClass {
 		// TODO Auto-generated method stub
   System.out.println("Arwaan Quazi");
   System.out.println("Saifaan Quazi");
+  System.out.println("Moin Quazi");
 	}
 
 }
